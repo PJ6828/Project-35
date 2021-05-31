0d7e9078efdcbd41f3efe17aea587b8cd6be17bd
PJ6828 <80190004+PJ6828@users.noreply.github.com>
2021-05-31 15:06:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var database,position;
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  database = firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+  var balloonRef = database.ref('balloon/position');
+  balloonRef.on("value",readPosition,showError);
+
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    update(-10,0);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    update(10,0);
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    update(0,-10);
+    balloon.scale = balloon.scale + 0.05;
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    update(0,10);
+    balloon.scale = balloon.scale - 0.05;
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+function update(x,y) {
+  database.ref('balloon/position').set({
+    x : balloon.x + x,
+    y : balloon.y + y
+  })
+}
+
+function readPosition(data) {
+  position = data.val();
+  balloon.x = position.x;
+  balloon.y = position.y;
+}
+
+function showError() {
+  console.log("Error 404: Not found");
+}
